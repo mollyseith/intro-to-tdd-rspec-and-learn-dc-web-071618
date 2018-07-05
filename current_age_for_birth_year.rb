99f19b8b0a2b94b4e9cd7dd859e2dbb7405ed29a
Molly Seither <molly.seither@centre.edu>
2018-07-05 15:41:26 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,5 +1,3 @@
-it "should return the current year for a person born in year 0" do
-  age = current_age_for_birth_year(0)
-
-  expect(age).to eq(2003)
+def current_age_for_birth_year(birth_year)
+  2003 - birth_year
 end
